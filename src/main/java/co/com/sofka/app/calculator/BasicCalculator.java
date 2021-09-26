76ee2049a1d6ff9f31f6673d12182a2a43a24029
Camilo U <cauribeva@unal.edu.co>
2021-09-26 18:55:05 -0500

    Added division method

--- src/main/java/co/com/sofka/app/calculator/BasicCalculator.java
+++ src/main/java/co/com/sofka/app/calculator/BasicCalculator.java
@@ -1,4 +1,34 @@
 package co.com.sofka.app.calculator;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class BasicCalculator {
+    private static final Logger logger = LoggerFactory.getLogger(BasicCalculator.class);
+
+    public Long sum(Long number1, Long number2) {
+        logger.info( "Summing {} + {}", number1, number2 );
+        return number1 + number2;
+    }
+    public Long subtract(Long number1, Long number2) {
+        logger.info( "Subtracting {} - {}", number1, number2 );
+        return number1 - number2;
+    }
+
+    public Long multiply(Long number1, Long number2) {
+        logger.info( "Multiplying {} by {}", number1, number2 );
+        return number1 * number2;
+    }
+
+    public Long divide(Long number1, Long number2) {
+        if(number2==0) {
+            logger.info("Division by zero is undefined");
+            throw new ArithmeticException();
+
+        }
+        logger.info( "Dividing {} by {}", number1, number2 );
+        return number1 / number2;
+    }
+
+
 }
